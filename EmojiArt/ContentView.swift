f71cbff6a1a6d749b40722a156ad5a1e51b4a628
theswiftkid <59027313+theswiftkid1@users.noreply.github.com>
2020-08-09 22:27:47 +0200

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by theswiftkid_ on 8/9/20.
+//  Copyright © 2020 theswiftkid_. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
